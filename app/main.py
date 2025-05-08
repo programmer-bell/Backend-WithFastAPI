5114ca5c26de166bd98e971142afe00407d969c6
Badhan Sarkar <badhansarkar164@gmail.com>
2025-05-08 23:03:12 +0530

    Fixing Routers

--- app/main.py
+++ app/main.py
@@ -1,19 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,APIRouter
+from app.api.v1 import endpoints
 from app.db.session import engine
 from app.db import base
-from app.api.v1.endpoints.users import router as users_router
 
 base.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="E-commerce Backend")
 
 @app.get("/")
-async def get():
-    return {"message":"Welcome to our Ecommerce FastAPI"}
+def home():
+    return {"message":"Welcome"}
 
-
-# Include all routers here:
-app.include_router(users_router, prefix="/api/v1")
+# Register routers
+app.include_router(endpoints.router, prefix="/api/v1")
 
 
 
